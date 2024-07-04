faed17ae20afdf2e358dcaa5ba91819bc4a93828
naelinda@gmail.com <naelinda@gmail.com>
2024-07-04 23:17:49 +0300

    put api as consts

--- src/api/verbit-api.ts
+++ src/api/verbit-api.ts
@@ -1,16 +1,18 @@
 import axios from "axios";
-import { Transcript } from "../Interfaces/Transcript";
+
+const GET_TRANSCRIPT_API: string = 'https://verbit-karaoke-assignment.vercel.app/api/transcripts';
+const GET_TRANSCRIPT_BY_ID_API: string = 'https://verbit-karaoke-assignment.vercel.app/api/transcripts/';
 
 export const retrieveTranscriptList = async () => {
     const response = await axios.get(
-      'https://verbit-karaoke-assignment.vercel.app/api/transcripts'
+      GET_TRANSCRIPT_API
     );
     return response.data;
   };
 
   export const retrieveTranscriptById = async (transcriptId: string | undefined) => {
     const response = await axios.get(
-        `https://verbit-karaoke-assignment.vercel.app/api/transcripts/${transcriptId}`
+      GET_TRANSCRIPT_BY_ID_API +transcriptId
     );
     return response.data;
   };
