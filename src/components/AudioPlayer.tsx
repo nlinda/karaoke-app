423fc2dc7de36122a560699545b2e10c4975ae90
naelinda@gmail.com <naelinda@gmail.com>
2024-07-05 00:06:15 +0300

    style the teanscript

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -4,21 +4,14 @@ import './AudioPlayer.css'; // Import your CSS file for styling
 interface AudioPlayerProps {
   audioUrl: string;
   audioRef: React.RefObject<HTMLVideoElement>;
-  // handlePlay:() => void ;
-  // handlePause: () => void;
-  // handleTimeUpdate:(time: number) => void ;
-  // handleVolumeChange: (volume: number) => void;
-  // handleSpeedChange: (speed: number) => void;
+  
 }
 
 const AudioPlayer: React.FC<AudioPlayerProps> = ({ audioUrl, audioRef}) => {
 
   return (
-    <div className="audio-player">
-      <video ref={audioRef} src={audioUrl} className="audio-element" 
-      // onTimeUpdate={() => handleTimeUpdate(audioRef.current?.currentTime || 0)} 
-      // onPlay={handlePlay}
-      // onPause={handlePause}
+    <div className="audio-player"  >
+      <video id="myVideo" ref={audioRef} src={audioUrl} className="audio-element" 
       controls
       />
     </div>
